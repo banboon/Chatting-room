19477124a355d6c9e76ca959bc0a94be9a00b933
Jian Yuan <yjian1027@gmail.com>
2015-04-14 13:34:39 -0400

    Added keyboard 'return' hit function. Added feature: After 'enter' hit, text line is cleared.

--- UI.py
+++ UI.py
@@ -2,10 +2,12 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
+
 class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.initUI()
+
 
     def initUI(self):
         self.setWindowTitle('Chatting room')
@@ -15,10 +17,12 @@ class MainWindow(QWidget):
 
         self.setupWidget()
 
+
     def setupWidget(self):
         self._layout = QVBoxLayout()
         #self._layout.setSpacing(10)
 
+        # Setup window for chatting history
         self._chatting_text = QTextEdit(self)
         self._chatting_text.setMinimumHeight(80)
         self._chatting_text.setStyleSheet('QTextEdit { font: 18px; } QTextEdit[class=invalid] { background-color: #FFCDCD; };')
@@ -30,27 +34,35 @@ class MainWindow(QWidget):
 
         hlayout = QHBoxLayout()
 
+        # Setup text line for entering text to be sent
         self._enter_text = QLineEdit(self)
         self._enter_text.setMinimumWidth(400)
 
         hlayout.addWidget(self._enter_text)
         hlayout.addStretch(1)
 
+        # Setup 'enter' button
         self.enter_button = QPushButton('Enter', self)
         self.enter_button.clicked.connect(self.execute)
         hlayout.addWidget(self.enter_button)
+
+        # Enable keyboard 'return' hit function
+        self._enter_text.returnPressed.connect(self.enter_button.clicked)
 
         self._layout.addLayout(hlayout)
 
         self.setLayout(self._layout)
 
+
     def run(self):
         self.show()
 
-    def execute(self):
-    	self.chatting_log = self._enter_text.text() if self.chatting_log is '' else self.chatting_log + '\n' + self._enter_text.text()
-    	self._chatting_text.setText(self.chatting_log)
 
+    def execute(self):
+        self.chatting_log = self._enter_text.text() if self.chatting_log is '' else self.chatting_log + '\n' + self._enter_text.text()
+        self._chatting_text.setText(self.chatting_log)
+        self._enter_text.setText('')
+        
 
 def main():
     app = QApplication(sys.argv)
