16c8287b9b3839f8e8a4bdcd395afc9c070d957e
Jian Yuan <yjian1027@gmail.com>
2015-04-16 13:26:53 -0400

    Rewrote the Client and Server.

--- Client.py
+++ Client.py
@@ -1,16 +1,47 @@
+import sys
 import socket
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = socket.gethostname()
+def client(argv):
+    if len(argv) < 2:
+        print("Usage: chat hostname")
+        sys.exit(1)
 
-port = 9876
+    HOST = argv[1]
+    PORT = 9876
 
-s.connect((host,port))
+    if len(argv) > 2: 
+        PORT = argv[2]  
 
-tm = s.recv(1024)
+    sockfd = None
+    res = socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM)
 
-s.close()
+    for one in res:
+        af, socktype, proto, canonname, sa = one
+        try:
+            sockfd = socket.socket(af, socktype, proto)
+        except OSError as msg:
+            sockfd = None
+            continue
+        try:
+            sockfd.connect(sa)
+        except OSError as msg:
+            sockfd.close()
+            sockfd = None
+            continue
+        break
 
-print('Time: %s' % tm.decode('ascii'))
+    if sockfd is None:
+        print('could not open socket')
+        sys.exit(2)
 
+    sockfd.sendall(b'Hello world')
+    data = sockfd.recv(1024)
+
+    sockfd.close()
+
+    print('Received', repr(data))
+
+
+if __name__ == '__main__':
+    client(sys.argv)
--- Server.py
+++ Server.py
@@ -1,23 +1,48 @@
+import sys
 import socket
 import time
 
 # create a socket project
+def server(argv):     
+    HOST = None     
+    PORT = 9876
 
-serversocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+    if len(argv) > 1:
+        PORT = argv[1]
 
-host = socket.gethostname()
+    sockfd = None
+    res = socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE)
 
-port = 9876
+    for one in res:
+        af, socktype, proto, canonname, sa = one
+        try:
+            sockfd = socket.socket(af, socktype, proto)
+        except OSError as msg:
+            sockfd = None
+            continue
+        try:
+            sockfd.bind(sa)
+            sockfd.listen(5)
+        except OSError as msg:
+            sockfd.close()
+            sockfd = None
+            continue
+        break
 
-serversocket.bind((host, port))
+    if sockfd is None:
+        print('could not open socket')
+        sys.exit(1)
 
-serversocket.listen(5)
+    conn, addr = sockfd.accept()
+    print('Connected by ', addr)
 
-while True:
+    while True:
+        data = conn.recv(1024)
+        if not data: break
+        conn.send(data)
 
-	clientSocket,addr = serversocket.accept()
+    sockfd.close()
 
-	print('A connect from :%s' % str(addr))
-	currentTime = time.ctime(time.time())+"\r\n"
-	clientSocket.send(currentTime.encode('ascii'))
-	clientSocket.close()
+
+if __name__ == '__main__':
+    server(sys.argv)
