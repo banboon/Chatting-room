18d8db7b673966453c7401ae39c2360eb7e4ac08
Jian Yuan <yjian1027@gmail.com>
2015-04-20 21:02:19 -0400

    Some little modification.

--- GuiChat/GuiServer.py
+++ GuiChat/GuiServer.py
@@ -4,30 +4,75 @@ from PySide.QtCore import *
 from PySide.QtNetwork import *
 
 
-class Server(QPushButton):
-    def __init__(self, parent = None):
-        super(Server, self).__init__('&Close Server', parent)
+class ServerWindow(QWidget):
+    def __init__(self):
+        super(ServerWindow, self).__init__()
+        self.setupUi()
 
         self.server = QTcpServer(self)
 
         self.client_sockets = []
         self.server.newConnection.connect(self.newConnection)
-        self.clicked.connect(self.close)
-
-        self.server.listen(QHostAddress('0.0.0.0'), 9876)
 
 
+    def setupUi(self):
+        self.layout = QVBoxLayout()
+
+        self.hlayout_1 = QHBoxLayout()
+
+        self.port_lbl = QLabel('Port: ', self)
+        self.hlayout_1.addWidget(self.port_lbl)
+
+        self.port_input = QLineEdit()
+        self.port_input.setMaxLength(5)
+        self.hlayout_1.addWidget(self.port_input)
+
+        self.layout.addLayout(self.hlayout_1)
+
+        self.hlayout_2 = QHBoxLayout()
+
+        self.start_btn = QPushButton('Start Server', self)
+        self.start_btn.clicked.connect(self.start)
+        self.hlayout_2.addWidget(self.start_btn)
+
+        self.close_btn = QPushButton('Close Server', self)
+        self.close_btn.clicked.connect(self.close)
+        self.hlayout_2.addWidget(self.close_btn)
+
+        self.layout.addLayout(self.hlayout_2)
+        
+        self.setLayout(self.layout)
+
+
+    @Slot()    
+    def start(self):
+        if self.port_input.text():
+            self.server.listen(QHostAddress('0.0.0.0'), int(self.port_input.text()))
+
+
+    def closeEvent(self, event):
+        for it in self.client_sockets:
+            if it.state() is not QAbstractSocket.ClosingState:
+                it.close()
+        self.close()
+        event.accept()
+
+
+    @Slot()
     def newConnection(self):
         client_sock = self.server.nextPendingConnection()
         client_sock.readyRead.connect(self.readyRead)
         self.client_sockets.append(client_sock)
 
 
+    @Slot()
     def readyRead(self):
         for sock in self.client_sockets:
             message = ''
-            while sock.canReadLine():
-                message = message + str(sock.readLine())
+            # while sock.canReadLine():
+            #     message = message + str(sock.readLine())
+            if sock.isReadable():
+                message = str(sock.readAll())
 
             for other in self.client_sockets:
                 if other is not sock:
@@ -36,11 +81,12 @@ class Server(QPushButton):
 
     def run(self):
         self.show()
+        #self.port_input.setFocus()
 
 
 def main(argv):
     app = QApplication(argv)
-    chat_server = Server()
+    chat_server = ServerWindow()
     chat_server.run()
     chat_server.move(0, 0)
     sys.exit(app.exec_())
